5f698029862731966c7a8a3daecc364d7cd91443
stanislike <s.joulah@outlook.fr>
2024-08-03 20:33:39 +0200

    finished

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
-// Créer un événement au click sur le hamburger menu pour sortir la sidebar, ranger la sidebar si on reclick dessus
+const sidebar = document.getElementById("side-bar");
+const content = document.querySelector(".content");
 
-// Ranger la sidebar si on click sur le contenu principal
+btn.addEventListener("click", () => {
+  sidebar.classList.toggle("active");
+});
+
+content.addEventListener("click", () => {
+  sidebar.classList.remove("active");
+});
 
 // BONUS
 // Créer en css l'hamburger menu qui se transforme en croix
 
 // Aide
-// Priorités en CSS : id > classe > baliseHtml
+// Priorités en CSS : !important > id > classe > baliseHtml
